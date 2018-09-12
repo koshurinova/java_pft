ef235a72cd7318d39b3fcfdc7bcc65ce1104497b
koshurinova <s.koshurinova@quality-lab.ru>
2018-09-12 21:54:22 +0300

    Классы, объекты, методы

--- sandbox/src/main/java/MyFirstPr.java
+++ sandbox/src/main/java/MyFirstPr.java
@@ -3,25 +3,21 @@ public class MyFirstPr {
     public static void main(String[] args) {
         hello("user");
         hello("world");
-        double len = 5;
-        double a = 6;
-        double b = 7;
-        System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
-        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b+ " = " + area(a, b));
+
+        Square s=new Square(5);
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+
+        Rectangle r=new Rectangle(7,8);
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b+ " = " + r.area());
     }
 
     public static void hello(String slovo) {
         System.out.println("Hello, " + slovo + "!");
     }
 
-    public static double area(double l) {
-        return l * l;
-    }
 
-    public static double area(double a, double b) {
-        return a * b;
 
-    }
+
 }
 
 
