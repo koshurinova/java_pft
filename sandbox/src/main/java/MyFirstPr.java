b9c9bd7f81518af1694d9d26549543b7eb2a731c
koshurinova <s.koshurinova@quality-lab.ru>
2018-09-12 21:21:47 +0300

    Добавление функций

--- sandbox/src/main/java/MyFirstPr.java
+++ sandbox/src/main/java/MyFirstPr.java
@@ -1,8 +1,27 @@
 public class MyFirstPr {
 
-public static void main(String[] args) {
-System.out.println("Hello,word!")
-    System.out.println();;
+    public static void main(String[] args) {
+        hello("user");
+        hello("world");
+        double len = 5;
+        double a = 6;
+        double b = 7;
+        System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b+ " = " + area(a, b));
+    }
+
+    public static void hello(String slovo) {
+        System.out.println("Hello, " + slovo + "!");
+    }
+
+    public static double area(double l) {
+        return l * l;
+    }
+
+    public static double area(double a, double b) {
+        return a * b;
+
+    }
 }
 
-}
+
