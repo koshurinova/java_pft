dda7c1a0f2e243a823eb9d2fd73dfdcf1eb91c1e
koshurinova <s.koshurinova@quality-lab.ru>
2018-09-13 20:45:23 +0300

    Домашнее задание 3_1. TestNG, тест Point

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTestes.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTestes.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.sandbox;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class PointTestes {
@@ -8,5 +9,6 @@ public class PointTestes {
         Point one = new Point(3,2);
         Point two = new Point(5,7);
         assert one.distance2(two) == 5.385164807134504;
+        Assert.assertEquals(one.distance2(two), 5.385164807134504);
     }
 }
