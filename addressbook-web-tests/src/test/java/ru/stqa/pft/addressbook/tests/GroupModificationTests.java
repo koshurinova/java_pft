53e92f7ef48e07ad48e9dcb6942f1a2cf85be924
koshurinova <s.koshurinova@quality-lab.ru>
2018-09-21 22:31:37 +0300

    GroupModification

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupModificationTests extends TestBase {
+
+    @Test
+    public void testGroupModification(){
+        app.getGroupHelper().goToGroupPage();
+        app.getGroupHelper().selectGroup(By.name("selected[]"));
+        app.getGroupHelper().initGroupModification();
+        app.getGroupHelper().fillGroupForm(new GroupData("test2", "test3", "test4"));
+        app.getGroupHelper().submitGroupModification();
+        app.getNavigationHelper().returnGroupPage();
+    }
+}
