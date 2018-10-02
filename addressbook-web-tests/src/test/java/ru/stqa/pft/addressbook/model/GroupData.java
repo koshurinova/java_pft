07e09d28ed06a6c8e3fa252298c6e1880c2d687a
koshurinova <s.koshurinova@quality-lab.ru>
2018-10-02 22:31:41 +0300

    Множества: неупорядоченные коллекции

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -4,13 +4,52 @@ import java.util.Objects;
 
 public class GroupData {
     private final String name;
+    private final String id;
     private final String header;
     private final String footer;
 
     public GroupData(String name, String header, String footer) {
+        this.id = null;
         this.name = name;
         this.header = header;
         this.footer = footer;
+    }
+
+    public GroupData(String id,String name, String header, String footer) {
+        this.id = id;
+        this.name = name;
+        this.header = header;
+        this.footer = footer;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        GroupData groupData = (GroupData) o;
+
+        if (name != null ? !name.equals(groupData.name) : groupData.name != null) return false;
+        return id != null ? id.equals(groupData.id) : groupData.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (id != null ? id.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "GroupData{" +
+                "name='" + name + '\'' +
+                ", id='" + id + '\'' +
+                '}';
     }
 
     public String getName() {
@@ -19,26 +58,6 @@ public class GroupData {
 
     public String getHeader() {
         return header;
-    }
-
-    @Override
-    public String toString() {
-        return "GroupData{" +
-                "name='" + name + '\'' +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        GroupData groupData = (GroupData) o;
-        return Objects.equals(name, groupData.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name);
     }
 
     public String getFooter() {
