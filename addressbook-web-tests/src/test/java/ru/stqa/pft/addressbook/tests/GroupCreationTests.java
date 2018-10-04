a686c62764f2f79baee4afbe7d18fd6181cabe11
koshurinova <s.koshurinova@quality-lab.ru>
2018-10-04 22:27:46 +0300

    Лямбда-выражения (анонимные функции) и элементы функционального программирования

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -13,18 +14,23 @@ public class GroupCreationTests extends TestBase{
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().goToGroupPage();
     List<GroupData> before=app.getGroupHelper().getGroupList();
-    GroupData group = new GroupData("test1", null, null);
+    GroupData group = new GroupData("test3", null, null);
     app.getGroupHelper().createGroup(group);
     List<GroupData> after=app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size()+1);
-    before.add(group);
-    int max=0;
-    for (GroupData g: after){
-      if (g.getId()>max) {
-        max=g.getId();
-      }
 
-    }
+//    int max=0;
+//    for (GroupData g: after){
+//      if (g.getId()>max) {
+//        max=g.getId();
+//      }
+//
+//    }
+
+//    Comparator<? super GroupData> byId= (Comparator<GroupData>) (o1, o2) -> Integer.compare(o1.getId(), o2.getId());
+    int max = after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId();
+
+    before.add(group);
     group.setId(max);
     Assert.assertEquals(new HashSet<>(before),new HashSet<>(after));
   }
