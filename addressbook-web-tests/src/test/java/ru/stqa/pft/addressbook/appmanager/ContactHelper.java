d784e2ef6b9df3db58cfaa3a3ff3656244ded6d5
koshurinova <s.koshurinova@quality-lab.ru>
2018-10-20 20:14:34 +0300

    ДЗ_13. Параметризация теста создания контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -31,14 +31,14 @@ public class ContactHelper extends HelperBase {
     public void fillContactForm(ContactData contactData, boolean creation) {
         type(By.name("firstname"), contactData.getFirstname());
         type(By.name("lastname"), contactData.getLastname());
-        type(By.name("home"), contactData.getHomePhone());
+//        type(By.name("home"), contactData.getHomePhone());
         type(By.name("mobile"), contactData.getMobilePhone());
-        type(By.name("work"), contactData.getWorkPhone());
-        type(By.name("email"), contactData.getEmail());
-        type(By.name("email2"), contactData.getEmail2());
-        type(By.name("email3"), contactData.getEmail3());
+//        type(By.name("work"), contactData.getWorkPhone());
+//        type(By.name("email"), contactData.getEmail());
+//        type(By.name("email2"), contactData.getEmail2());
+//        type(By.name("email3"), contactData.getEmail3());
         type(By.name("address"), contactData.getAddress());
-        attach(By.name("photo"), contactData.getPhoto());
+//        attach(By.name("photo"), contactData.getPhoto());
 
 
 
